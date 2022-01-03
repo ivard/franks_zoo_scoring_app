ef614731c04723ed1d1634e21a40b652a41a986b
Ivar Derksen <ivarderksen@hotmail.nl>
2022-01-03 17:47:24 +0100

    Added scoring test for 5 players

--- test/scoring_test.dart
+++ test/scoring_test.dart
@@ -61,4 +61,57 @@ void main() {
     expect(score.getRanking(), ['alpha', 'gamma', 'beta', 'delta']);
     expect(players.map((p) => score.getScore(p)), [14, 10, 13, 3]);
   });
+
+  test('scoring-5', () {
+    final players = ['alpha', 'beta', 'gamma', 'delta', 'epsilon'];
+    GameScore score = GameScore(players: players.toSet());
+    expect(players.map((p) => score.getScore(p)), List.filled(5, 0));
+
+    score = score.addRound(GameRound(
+      result: ['alpha', 'beta', 'gamma', 'delta', 'epsilon'],
+    ));
+    expect(score.getRanking(), ['alpha', 'beta', 'gamma', 'delta', 'epsilon']);
+    expect(players.map((p) => score.getScore(p)), [5, 4, 3, 2, 0]);
+
+    Pairs pairs = score.getPairsForNextRound()!;
+    players
+        .where((p) => p != 'gamma')
+        .forEach((p) => expect(pairs.hasTeamMate(p), true));
+    expect(pairs.hasTeamMate('gamma'), false);
+    expect(pairs.getTeamMate('alpha'), 'delta');
+    expect(pairs.getTeamMate('beta'), 'epsilon');
+    expect(pairs.getTeamMate('delta'), 'alpha');
+    expect(pairs.getTeamMate('epsilon'), 'beta');
+
+    score = score.addRound(GameRound(
+      result: ['gamma', 'beta', 'alpha', 'delta', 'epsilon'],
+      pairs: pairs,
+    ));
+    expect(score.getRanking(), ['gamma', 'alpha', 'beta', 'delta', 'epsilon']);
+    expect(players.map((p) => score.getScore(p)), [9, 7, 11, 6, 3]);
+
+    pairs = score.getPairsForNextRound()!;
+    players
+        .where((p) => p != 'beta')
+        .forEach((p) => expect(pairs.hasTeamMate(p), true));
+    expect(pairs.hasTeamMate('beta'), false);
+    expect(pairs.getTeamMate('alpha'), 'epsilon');
+    expect(pairs.getTeamMate('gamma'), 'delta');
+    expect(pairs.getTeamMate('delta'), 'gamma');
+    expect(pairs.getTeamMate('epsilon'), 'alpha');
+
+    final round = GameRound(
+      result: ['epsilon', 'alpha', 'gamma', 'beta', 'delta'],
+      pairs: pairs,
+    );
+    round.setHasHedgehogs('delta', true);
+    round.setHasHedgehogs('epsilon', true);
+    round.setLionsInHandLastPlayer(1);
+    round.setNumberOfLions('delta', 1);
+    round.setNumberOfLions('epsilon', 2);
+    round.validate();
+    score = score.addRound(round);
+    expect(score.getRanking(), ['alpha', 'epsilon', 'gamma', 'beta', 'delta']);
+    expect(players.map((p) => score.getScore(p)), [17, 12, 13, 8, 14]);
+  });
 }
